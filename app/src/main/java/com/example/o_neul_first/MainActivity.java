85d8436e00b2c811e6578b94810fa41831cabf35
Sondouni <ezicland@gmail.com>
2022-02-28 16:24:41 +0900

    메인화면 + FoodActivity

--- app/src/main/java/com/example/o_neul_first/MainActivity.java
+++ app/src/main/java/com/example/o_neul_first/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.o_neul_first;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView food_img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        food_img = findViewById(R.id.food_img);
+
+
+
+
+        food_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,FoodActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
